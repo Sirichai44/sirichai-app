9eedefcaf97767a4677d86de1fb834b300ed6379
Sirichai99 <mercier_1999@hotmail.com>
2024-04-06 23:22:20 +0700

    set up tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,32 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { Dispatch } from "redux";
+import { useDispatch } from "react-redux";
 
-import reactLogo from "@/assets/react.svg";
-import viteLogo from "@/assets/vite.svg";
-import "@/styles/App.css";
+// import { toast } from "react-toastify";
+import * as Action from "@/store/actions/action";
+// import * as Type from "@/store/typings/type";
 
 const index = () => {
-  const [count, setCount] = useState(0);
+  const dispatch = useDispatch<Dispatch<Action.Action>>();
+  // const state = useSelector((state: Type.IStore) => state.test);
+
+  const word = `%c
+  ██████╗  ██╗███╗   ██╗ ██████╗       ██╗██████╗ 
+  ██╔════╝ ██║████╗  ██║██╔═══██╗     ██╔╝╚════██╗
+  ██║  ███╗██║██╔██╗ ██║██║   ██║    ██╔╝  █████╔╝
+  ██║   ██║██║██║╚██╗██║██║   ██║    ╚██╗  ╚═══██╗
+  ╚██████╔╝██║██║ ╚████║╚██████╔╝     ╚██╗██████╔╝
+   ╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝       ╚═╝╚═════╝\n\nPlease like and follow me on github: https://github.com/Sirichai44
+`;
+  useEffect(() => {
+    console.log(word, "font-family:monospace;color:#1976d2;font-size:12px;");
+    dispatch({ type: "TEST_HELLO_SAGA", payload: "Hello World" });
+    dispatch({ type: "TEST_ACTION", payload: "Hello World" });
+  }, []);
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex items-center justify-center w-full h-screen min-h-full">
+      <div className="w-5/6 border border-red-500 h-4/6">Hello</div>
+    </div>
   );
 };
 
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,12 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Home from "@/pages/Home";
 import ErrorPage from "@/pages/ErrorPage";
+import App from "@/App";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <App />,
     errorElement: <ErrorPage />,
+    children: [{ path: "/", element: <Home /> }],
   },
 ]);
 
