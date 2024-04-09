f9abce50fe686641242e79b7fde1b37b1861e97a
Sirichai99 <mercier_1999@hotmail.com>
2024-04-09 23:30:24 +0700

    develop login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,24 @@
 import { useForm } from 'react-hook-form';
 import { Input, Button } from '@mui/joy';
 
-import AndroidRoundedIcon from '@mui/icons-material/AndroidRounded';
-
 const Login = () => {
   const form = useForm();
   return (
     <div>
-      {/* <AndroidRoundedIcon style={{ fontSize: 50 }} /> */}
+      <div className="cat">
+        <div className="ear ear--left"></div>
+        <div className="ear ear--right"></div>
+        <div className="face">
+          <div className="eye eye--left">
+            <div className="eye-pupil"></div>
+          </div>
+          <div className="eye eye--right">
+            <div className="eye-pupil"></div>
+          </div>
+          <div className="muzzle"></div>
+        </div>
+      </div>
+
       <form className="flex flex-col justify-center">
         <Input
           className="my-3"
