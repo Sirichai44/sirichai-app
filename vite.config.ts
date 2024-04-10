e094fa404fa56997d652fba1f925b8fbc0542f5c
Sirichai99 <mercier_1999@hotmail.com>
2024-04-10 23:52:33 +0700

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(new URL('.', import.meta.url).pathname, './src')
+      '@': path.resolve(__dirname, './src')
     }
   }
 });
