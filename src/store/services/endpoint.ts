3e400414e2b0c721a677bb774fd708241af09ec9
Sirichai99 <mercier_1999@hotmail.com>
2024-04-19 22:49:36 +0700

    dev

--- src/store/services/endpoint.ts
+++ src/store/services/endpoint.ts
@@ -3,7 +3,6 @@ const apiVersion = 'v3';
 const auth = 'auth';
 
 export const dev = 'http://localhost:3000';
-// export const prod = 'https://sirichai-app.onrender.com';
-export const prod = 'https://backend-sirichai-app.up.railway.app';
+export const prod = 'https://sirichai-app.onrender.com';
 export const Register = `${api}/${apiVersion}/${auth}/register`;
 export const Login = `${api}/${apiVersion}/${auth}/login`;
