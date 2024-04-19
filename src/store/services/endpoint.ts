f2b5cc3c0a5c7949ad386954fe8a5d5c89c5d22b
Sirichai99 <mercier_1999@hotmail.com>
2024-04-19 08:37:23 +0700

    develop

--- src/store/services/endpoint.ts
+++ src/store/services/endpoint.ts
@@ -1,6 +1,8 @@
 const api = '/api';
 const apiVersion = 'v3';
-
 const auth = 'auth';
+
+export const dev = 'http://localhost:3000';
+export const prod = 'https://sirichai-app.onrender.com';
 export const Register = `${api}/${apiVersion}/${auth}/register`;
 export const Login = `${api}/${apiVersion}/${auth}/login`;
